fc9f3e8729d5c15bceea8d5ceb801d0daaacc577
Goethe Carvalho <goethecarvalho@gmail.com>
2024-04-09 15:53:55 -0300

    Condicionais e fluxo de execução - Operadores lógicos encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -1,25 +1,33 @@
 console.log(`Trabalhando com condicionais`);
 
 const listaDeDestinos = new Array(`Salvador`,`São Paulo`,`Rio de Janeiro`);
-
 const idadeComprador = 17;
-
 const estaAcompanhada = true;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis:");
 
 console.log(listaDeDestinos);
 
-if(idadeComprador >= 18){
-    console.log("Compra efetuada com sucesso!");
-    listaDeDestinos.splice(1,1); // removendoelse{} item da lista
-}else{
-    if(estaAcompanhada){
+if(idadeComprador >= 18 || estaAcompanhada){
+    if(idadeComprador >= 18){
+        console.log("Compra efetuada com sucesso!");
+        listaDeDestinos.splice(1,1); // removendoelse{} item da lista
+    }else if(estaAcompanhada){
         console.log("Comprador está acompanhado!");
         listaDeDestinos.splice(1,1); // removendoelse{} item da lista
     }else{
         console.log("Compra não efetuada. Comprador menor de idade!");
     }
 }
+
+console.log("Embarque: \n");
+
+if(idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa viagem!");
+}else{
+    console.log("Embarque não permitido!");
+}
+
 
 console.log(listaDeDestinos);
