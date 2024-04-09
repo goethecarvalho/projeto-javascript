b15fdd1ea3601fe6f49664edc113a0169990a32f
Goethe Carvalho <goethecarvalho@gmail.com>
2024-04-09 15:13:45 -0300

    Listas

---
+++ 6-listas.js
@@ -0,0 +1,11 @@
+console.log(`Trabalhando com listas`);
+
+const listaDeDestinos = new Array(`Salvador`,`São Paulo`,`Rio de Janeiro`);
+
+console.log("Destinos possíveis:");
+
+console.log(listaDeDestinos);
+
+listaDeDestinos.push(`Curitiba`);
+
+console.log(listaDeDestinos);
