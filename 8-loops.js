d0a2bed4a0cb263aa60b440fc31d95287928e26d
Goethe Carvalho <goethecarvalho@gmail.com>
2024-04-12 09:13:59 -0300

    While - Break e Debbuger

--- 8-loops.js
+++ 8-loops.js
@@ -12,12 +12,14 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada ==  true;
 
 let contador = 0;
+let destinoExiste = false;
 
 while(contador < 3) {
     if(listaDeDestinos[contador] == destino){
-        console.log("Destino existe");
-    }else{
-        console.log("Destino não existe");
+        destinoExiste = true;
+        break;
     }
     contador += 1;
 }
+
+console.log("Destino existe: ", destinoExiste);
